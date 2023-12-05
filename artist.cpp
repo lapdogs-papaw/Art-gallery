5ce98da58faf4713301cf94d93f1e33099a3b50e
Greg W <152864600+lapdogs-papaw@users.noreply.github.com>
2023-12-05 13:36:30 +0600

    Create artist.cpp

---
+++ artist.cpp
@@ -0,0 +1,9 @@
+#include "artist.h"
+
+Artist::Artist() {
+    std::cout << "Artist created." << std::endl;
+}
+
+void Artist::displayArtist() {
+    std::cout << "Displaying artist information." << std::endl;
+}
