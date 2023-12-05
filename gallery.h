d9e27facda2781c1a3ef537b9c15dc9b2d04d6f5
Greg W <152864600+lapdogs-papaw@users.noreply.github.com>
2023-12-05 13:35:24 +0600

    Create gallery.h

---
+++ gallery.h
@@ -0,0 +1,15 @@
+#ifndef GALLERY_H
+#define GALLERY_H
+
+#include "artwork.h"
+
+class Gallery {
+public:
+    Gallery();
+    void runGallery();
+
+private:
+    Artwork artwork;
+};
+
+#endif // GALLERY_H
