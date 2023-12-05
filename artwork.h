652c82722b398d4d1f066c124cf8da0ecf63eb48
Greg W <152864600+lapdogs-papaw@users.noreply.github.com>
2023-12-05 13:35:50 +0600

    Create artwork.h

---
+++ artwork.h
@@ -0,0 +1,15 @@
+#ifndef ARTWORK_H
+#define ARTWORK_H
+
+#include "artist.h"
+
+class Artwork {
+public:
+    Artwork();
+    void displayArtwork();
+
+private:
+    Artist artist;
+};
+
+#endif // ARTWORK_H
